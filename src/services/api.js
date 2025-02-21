0c210fae466322ea9919061561f41f83acc86766
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-22 02:48:44 +0300

    An API service was created for API requests via mockAPI.

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://67b8ffa951192bd378dc679d.mockapi.io/secure/SpDWa22/api/v1/showcase/data',
+});
+
+
+export default api;
