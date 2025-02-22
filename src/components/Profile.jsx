6ed8071ec2e2529193583751ee9ff65ffe28cc0b
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-22 19:57:34 +0300

    In components; separator was used to determine the areas on the page.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { useTheme } from "../context/ThemeContext";
+import useFetch from '../hooks/useFetch';
 
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+    const { theme } = useTheme();
+    const { data, loading, error } = useFetch("/", "profile");
 
-export default Profile
+    if (loading) return <p className="text-center text-gray-600">Loading...</p>;
+    if (error) return <p className="text-center text-red-500">Error: {error}</p>;
+
+    return (
+        <div className={`p-8 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+            <h2 className={`text-5xl font-semibold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#1F2937]'} text-left`}>
+                Profile
+            </h2>
+
+            <div className="flex justify-between mt-8 space-x-8">
+                <div className="w-1/2">
+                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>Profile</h3>
+                    <div className="mt-4 space-y-4">
+                        <div className="flex">
+                            <p className="font-semibold text-lg">Doğum Tarihi</p>
+                            <p className="font-normal text-lg ml-2">{data?.birthDate}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="font-semibold text-lg">İkamet Şehri</p>
+                            <p className="font-normal text-lg ml-2">{data?.city}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="font-semibold text-lg">Eğitim Durumu</p>
+                            <p className="font-normal text-lg ml-2">{data?.education}</p>
+                        </div>
+
+                        <div className="flex">
+                            <p className="font-semibold text-lg">Tercih Ettiği Rol</p>
+                            <div className="font-normal text-lg ml-2">
+                                {data?.preferredRoles?.map((role, index) => (
+                                    <span key={index} className="mr-4">{role}</span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-1/2">
+                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>About Me</h3>
+                    <div className="mt-4">
+                        <p className="font-normal text-lg mb-6">
+                            {data?.aboutMe || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+                        </p>
+                        <p className="font-normal text-lg">
+                            {data?.aboutMe2 || 'Minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!'}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`mt-20 mb-10 border-b-2 ${theme === 'dark' ? 'border-[#BAB2E]' : 'border-[#BAB2E7]'}`}></div>
+        </div>
+    );
+};
+
+export default Profile;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import useFetch from "../hooks/useFetch";
+import { useTheme } from "../context/ThemeContext";
 
 const Skills = () => {
-    return (
-        <div>Skills</div>
-    )
-}
+    const { data, loading, error } = useFetch("/", "skills");
+    const { theme } = useTheme();
 
-export default Skills
+    if (loading) return <p className="text-center text-gray-600">Loading...</p>;
+    if (error) return <p className="text-center text-red-500">Error: {error}</p>;
+
+    return (
+        <div className={`p-8 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+            <h2 className={`text-5xl font-semibold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#34371f]'} text-left`}>
+                Skills
+            </h2>
+
+            <div className="flex gap-x-8 mt-8">
+                {data?.map((skill, index) => (
+                    <div key={index} className="w-1/3 text-left">
+                        <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'} text-left`}>
+                            {skill.name}
+                        </h3>
+                        <p className={`text-xs ${theme === 'dark' ? 'text-[#FFFFFF]' : 'text-[#6B7280]'} mt-2 text-left overflow-hidden line-clamp-3`}>
+                            {skill.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
+
+            <div className={`mt-20 mb-10 border-b-2 ${theme === 'dark' ? 'border-[#BAB2E]' : 'border-[#BAB2E7]'}`}></div>
+        </div>
+    );
+};
+
+export default Skills;
