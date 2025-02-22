c79d4c322032503ae58afe7ad6be85cbadd789d5
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-22 19:33:57 +0300

    The projects component has been updated for the light theme.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h2 className="text-3xl font-bold text-left">Projects</h2>
+            <h2 className="text-5xl font-bold text-left">Projects</h2>
             <div className="flex justify-between mt-6">
                 {data.map((project, index) => (
                     <div key={index} className="w-1/3 px-4">
@@ -18,15 +18,15 @@ const Projects = () => {
                             alt={project.title}
                             className="w-full h-auto mb-4"
                         />
-                        <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-                        <p className="text-gray-600 mb-4">{project.description}</p>
+                        <h3 className="text-3xl font-semibold mb-2 text-[#4338CA]">{project.title}</h3>
+                        <p className="text-sm font-normal text-[#6B7280] mb-4">{project.description}</p>
 
                         <nav className="flex space-x-5">
                             <a
                                 href="https://react.dev/"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50"
+                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
                             >
                                 React
                             </a>
@@ -35,7 +35,7 @@ const Projects = () => {
                                 href="https://redux.js.org/"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50"
+                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
                             >
                                 Redux
                             </a>
@@ -44,11 +44,31 @@ const Projects = () => {
                                 href="https://axios-http.com/docs/intro"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50"
+                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
                             >
                                 Axios
                             </a>
                         </nav>
+
+                        <div className="mt-4 flex justify-between">
+                            <a
+                                href={project.githubUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-base font-medium text-[#3730A3] underline"
+                            >
+                                GitHub
+                            </a>
+
+                            <a
+                                href={project.siteUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-base font-medium text-[#3730A3] underline"
+                            >
+                                View Site
+                            </a>
+                        </div>
                     </div>
                 ))}
             </div>
