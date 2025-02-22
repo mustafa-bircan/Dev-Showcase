44acaf01b62fe4a5efb21ebd60623f873a31239e
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 00:40:17 +0300

    Language selLanguage selection will be determined by end-pointection will be determined by end-point
    If data is Turkish, we will bring English data.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://67b8ffa951192bd378dc679d.mockapi.io/secure/SpDWa22/api/v1/showcase/data',
+    baseURL: 'https://67b8ffa951192bd378dc679d.mockapi.io/secure/SpDWa22/api/v1/showcase',
 });
 
 
