29e3e011c37b3a0e3afd62d739218924df1b6c3f
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-22 16:43:40 +0300

    Header component completed for light theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,22 @@ const Header = () => {
     return (
         <header className={`flex justify-between items-center p-4 ${theme === 'dark' ? 'bg-gray-800' : 'bg-white'}`}>
             <div className="flex items-center">
-                <div className="w-12 h-12 rounded-full bg-blue-500 flex justify-center items-center transform rotate-12">
-                    <span className="text-white font-bold text-xl">M</span>
+                <div className="w-12 h-12 rounded-full bg-[#EEEBFF] flex justify-center items-center transform rotate-12">
+                    <span className="text-[#7B61FF] font-bold text-xl">M</span>
                 </div>
+
             </div>
 
             <nav className="flex space-x-5">
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-black'} `}>
+                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-[#6B7280]'} `}>
                     Skills
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-black'} `}>
+                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-[#6B7280]'} `}>
                     Projects
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md ${theme === 'dark' ? 'text-white border-white' : 'text-black border-black'} border-opacity-50`}>
+                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md ${theme === 'dark' ? 'text-white border-white' : 'text-[#3730A3] border-[#3730A3]'} border-opacity-50`}>
                     Hire Me
                 </button>
             </nav>
