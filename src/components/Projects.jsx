80cd482769fa0397e5cbac89d7e51cfcfcf7651b
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-22 15:24:16 +0300

    Projects component created
    The necessary information from the API service was displayed as desired in the relevant component.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 const Projects = () => {
-    return (
-        <div>Projects</div>
-    )
-}
+    const { data, loading, error } = useFetch("/", "projects");
 
-export default Projects
+    if (loading) return <p className="text-center text-gray-600">Loading...</p>;
+    if (error) return <p className="text-center text-red-500">Error: {error}</p>
+
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold text-left">Projects</h2>
+            <div className="flex justify-between mt-6">
+                {data.map((project, index) => (
+                    <div key={index} className="w-1/3 px-4">
+                        <img
+                            src={project.imageUrl}
+                            alt={project.title}
+                            className="w-full h-auto mb-4"
+                        />
+                        <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+                        <p className="text-gray-600 mb-4">{project.description}</p>
+
+                        <nav className="flex space-x-5">
+                            <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50`}>
+                                Skills
+                            </button>
+
+                            <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50`}>
+                                Projects
+                            </button>
+
+                            <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md border-opacity-50`}>
+                                Hire Me
+                            </button>
+                        </nav>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
