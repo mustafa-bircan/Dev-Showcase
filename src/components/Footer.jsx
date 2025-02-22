07f4f952e7c988b58b568b61a351b73fe7fb25ff
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 00:42:21 +0300

    Language Selection for Footer Component
    We brought the correct data according to the language selection in the Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
     return (
         <footer className={`w-full py-8 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-[#F9F9F9] text-black'} mt-auto`}>
             <div className="container mx-auto flex justify-between items-center px-4">
-                <div className="flex flex-col items-start space-y-4">
+                <div className="flex flex-col items-start space-y-4 gap-20 pt-20 pb-20 pl-20">
                     <p className="text-5xl font-semibold">
                         {data?.heading}
                         <span className="text-5xl font-semibold block mt-2">{data?.heading2}</span>
@@ -24,9 +24,9 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="flex space-x-8 items-center">
-                    <a href={data?.personalBlog} className="text-lg font-medium text-[#0A0A14] hover:underline">
-                        Kişisel Blog
+                <div className="flex space-x-8 items-center pt-30 pr-20">
+                    <a href={data?.personalBlogUrl} className="text-lg font-medium text-[#0A0A14] hover:underline">
+                        {data?.personalBlog}
                     </a>
                     <a href={data?.github} className="text-lg font-medium text-[#00AB6B] hover:underline">
                         Github
