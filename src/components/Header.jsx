8df025c0953dfdcb95bce4e5403c07fc14c7d581
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 23:10:48 +0300

    When the buttons are clicked, transition to related components is provided
    We made the specified component visible by scrolling with the scrollIntoView() function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,27 +6,52 @@ const Header = () => {
     const { data } = useFetch('/', 'header');
     const { theme } = useTheme();
 
+    const scrollSkills = () => {
+        const skillsSection = document.getElementById("skills-section");
+        if (skillsSection) {
+            skillsSection.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    const scrollProjects = () => {
+        const projectsSection = document.getElementById("projects-section");
+        if (projectsSection) {
+            projectsSection.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
-        <header className={`flex justify-between items-center p-4 ${theme === 'dark' ? 'bg-gray-800' : 'bg-white'}`}>
+        <header className="flex justify-between items-center p-4">
             <div className="flex items-center">
-                <div className={`w-12 h-12 rounded-full ${theme === 'dark' ? 'bg-[#4731D3]' : 'bg-[#EEEBFF]'} flex justify-center items-center transform rotate-12`}>
+                <div className={`w-12 h-12 rounded-full ${theme === 'dark' ? 'bg-[#4731D3]' : 'bg-[#EEEBFF]'} flex justify-center items-center transform rotate-20`}>
                     <span className={`${theme === 'dark' ? 'text-[#8F88FF]' : 'text-[#7B61FF]'} font-bold text-xl`}>M</span>
                 </div>
             </div>
 
-            <nav className="flex space-x-5">
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-[#6B7280]' : 'text-[#6B7280]'}`}>
+            <nav className="flex space-x-10">
+                <button className={`py-2 px-4 text-lg font-semibold 
+                ${theme === 'dark' ? 'text-[#E1E1FF] hover:border-[#E1E1FF] hover:text-[#E1E1FF]' : 'text-[#6B7280]'}
+                border-2 border-transparent hover:border-[#3730A3] hover:text-[#3730A3] rounded-md transition-all duration-800`}
+                    onClick={scrollSkills}
+                >
                     {data?.hSkills}
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-[#6B7280]' : 'text-[#6B7280]'}`}>
+                <button className={`py-2 px-4 text-lg font-semibold 
+                ${theme === 'dark' ? 'text-[#E1E1FF] hover:border-[#E1E1FF] hover:text-[#E1E1FF]' : 'text-[#6B7280]'}
+                border-2 border-transparent hover:border-[#3730A3] hover:text-[#3730A3] rounded-md transition-all duration-800`}
+                    onClick={scrollProjects}
+                >
                     {data?.hProjects}
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md ${theme === 'dark' ? 'bg-white text-[#3730A3] border-white' : 'bg-[#FFFFFF] text-[#3730A3] border-[#3730A3]'} border-opacity-50`}>
+                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md 
+                ${theme === 'dark' ? 'bg-[#E1E1FF] text-[#3730A3] border-white' : 'bg-[#FFFFFF] text-[#3730A3] border-[#3730A3]'}`}
+                >
                     {data?.hHireMe}
                 </button>
             </nav>
+
         </header>
     );
 };
