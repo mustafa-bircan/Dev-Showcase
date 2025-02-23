0318efbadbcd8d9e9b9e4db11f730ba494ca75ff
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 16:24:43 +0300

    Header component designed for dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,26 +6,24 @@ const Header = () => {
     const { data } = useFetch('/', 'header');
     const { theme } = useTheme();
 
-
     return (
         <header className={`flex justify-between items-center p-4 ${theme === 'dark' ? 'bg-gray-800' : 'bg-white'}`}>
             <div className="flex items-center">
-                <div className="w-12 h-12 rounded-full bg-[#EEEBFF] flex justify-center items-center transform rotate-12">
-                    <span className="text-[#7B61FF] font-bold text-xl">M</span>
+                <div className={`w-12 h-12 rounded-full ${theme === 'dark' ? 'bg-[#4731D3]' : 'bg-[#EEEBFF]'} flex justify-center items-center transform rotate-12`}>
+                    <span className={`${theme === 'dark' ? 'text-[#8F88FF]' : 'text-[#7B61FF]'} font-bold text-xl`}>M</span>
                 </div>
-
             </div>
 
             <nav className="flex space-x-5">
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-[#6B7280]'} `}>
+                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-[#6B7280]' : 'text-[#6B7280]'}`}>
                     {data?.hSkills}
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-[#6B7280]'} `}>
+                <button className={`py-2 px-4 text-lg font-semibold ${theme === 'dark' ? 'text-[#6B7280]' : 'text-[#6B7280]'}`}>
                     {data?.hProjects}
                 </button>
 
-                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md ${theme === 'dark' ? 'text-white border-white' : 'text-[#3730A3] border-[#3730A3]'} border-opacity-50`}>
+                <button className={`py-2 px-4 text-lg font-semibold border-2 rounded-md ${theme === 'dark' ? 'bg-white text-[#3730A3] border-white' : 'bg-[#FFFFFF] text-[#3730A3] border-[#3730A3]'} border-opacity-50`}>
                     {data?.hHireMe}
                 </button>
             </nav>
