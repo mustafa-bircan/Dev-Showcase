def6e543fe9179ecd7539e6624681c07b5d467fb
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 13:41:54 +0300

    All profile component information was received dynamically via end point
    Correspondences were given in the json file according to the language selection

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,30 +12,30 @@ const Profile = () => {
     return (
         <div className={`${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
             <h2 className={`text-5xl font-semibold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#1F2937]'} text-left`}>
-                Profil
+                {data?.title}
             </h2>
 
             <div className="flex justify-between mt-8 space-x-8">
                 <div className="w-1/2">
-                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>Profil</h3>
+                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>{data?.title2}</h3>
                     <div className="mt-4 space-y-4">
                         <div className="flex">
-                            <p className="font-semibold text-lg">Doğum Tarihi</p>
+                            <p className="font-semibold text-lg">{data?.birtDateT}</p>
                             <p className="font-normal text-lg ml-10">{data?.birthDate}</p>
                         </div>
 
                         <div className="flex">
-                            <p className="font-semibold text-lg">İkamet Şehri</p>
+                            <p className="font-semibold text-lg">{data?.cityResidence}</p>
                             <p className="font-normal text-lg ml-12">{data?.city}</p>
                         </div>
 
                         <div className="flex">
-                            <p className="font-semibold text-lg">Eğitim Durumu</p>
+                            <p className="font-semibold text-lg">{data?.educationI}</p>
                             <p className="font-normal text-lg ml-7.5">{data?.education}</p>
                         </div>
 
                         <div className="flex">
-                            <p className="font-semibold text-lg">Tercih Ettiği Rol</p>
+                            <p className="font-semibold text-lg">{data?.preferredRolesT}</p>
                             <div className="font-normal text-lg ml-6">
                                 {data?.preferredRoles?.map((role, index) => (
                                     <span key={index} className="mr-2">{role}</span>
@@ -46,7 +46,7 @@ const Profile = () => {
                 </div>
 
                 <div className="w-1/2">
-                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>Hakkımda</h3>
+                    <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'}`}>{data?.about}</h3>
                     <div className="mt-4">
                         <p className="font-normal text-lg mb-6 text-[#6B7280]">
                             {data?.aboutMe || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
@@ -58,7 +58,7 @@ const Profile = () => {
                 </div>
             </div>
 
-            <div className={`mt-15 mb-5 border-b-2 ${theme === 'dark' ? 'border-[#BAB2E]' : 'border-[#BAB2E7]'}`}></div>
+            <div className={`mt-12 mb-12 border-b-2 ${theme === 'dark' ? 'border-[#BAB2E]' : 'border-[#BAB2E7]'}`}></div>
         </div>
     );
 };
