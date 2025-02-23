f1a6fb33710bf650ed468915945ec36163fd6ac0
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 21:43:41 +0300

    Projects component updated for dark mode

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
+import { useTheme } from "../context/ThemeContext";
 
 const Projects = () => {
-    const { data, loading, error } = useFetch("/", "projects");
-
-    if (loading) return <p className="text-center text-gray-600">Loading...</p>;
-    if (error) return <p className="text-center text-red-500">Error: {error}</p>;
+    const { data } = useFetch("/", "projects");
+    const { theme } = useTheme();
 
     return (
         <div className="container mx-auto">
-            <h2 className="text-5xl font-bold text-left">{data?.projectT}</h2>
+            <h2 className={`text-5xl font-bold text-left ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-black'}`}>
+                {data?.projectT}
+            </h2>
             <div className="flex justify-between gap-5 mt-12">
                 {data?.items?.map((project, index) => (
                     <div key={index} className="w-1/3 px-4">
@@ -18,15 +19,19 @@ const Projects = () => {
                             alt={project?.title}
                             className="w-full h-auto mb-4"
                         />
-                        <h3 className="text-3xl font-semibold mb-2 text-[#4338CA]">{project?.title}</h3>
-                        <p className="text-sm font-normal text-[#6B7280] mb-4">{project?.description}</p>
+                        <h3 className={`text-3xl font-semibold mb-2 ${theme === 'dark' ? 'text-[#CFCBFF]' : 'text-[#4338CA]'}`}>
+                            {project?.title}
+                        </h3>
+                        <p className={`text-sm font-normal mb-4 ${theme === 'dark' ? 'text-[#FFFFFF]' : 'text-[#6B7280]'}`}>
+                            {project?.description}
+                        </p>
 
                         <nav className="flex space-x-5">
                             <a
                                 href="https://react.dev/"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
+                                className={`py-1 px-3 text-sm font-medium border-2 rounded-md border-opacity-50 ${theme === 'dark' ? 'bg-[#383838] text-[#8F88FF]' : 'text-[#3730A3]'}`}
                             >
                                 React
                             </a>
@@ -35,7 +40,7 @@ const Projects = () => {
                                 href="https://redux.js.org/"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
+                                className={`py-1 px-3 text-sm font-medium border-2 rounded-md border-opacity-50 ${theme === 'dark' ? 'bg-[#383838] text-[#8F88FF]' : 'text-[#3730A3]'}`}
                             >
                                 Redux
                             </a>
@@ -44,7 +49,7 @@ const Projects = () => {
                                 href="https://axios-http.com/docs/intro"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="py-1 px-3 text-sm font-medium text-[#3730A3] border-2 rounded-md border-opacity-50"
+                                className={`py-1 px-3 text-sm font-medium border-2 rounded-md border-opacity-50 ${theme === 'dark' ? 'bg-[#383838] text-[#8F88FF]' : 'text-[#3730A3]'}`}
                             >
                                 Axios
                             </a>
@@ -55,7 +60,7 @@ const Projects = () => {
                                 href={project?.githubUrl}
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="text-base font-medium text-[#3730A3] underline"
+                                className={`text-base font-medium underline ${theme === 'dark' ? 'text-[#E1E1FF]' : 'text-[#3730A3]'}`}
                             >
                                 GitHub
                             </a>
@@ -64,7 +69,7 @@ const Projects = () => {
                                 href={project?.siteUrl}
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="text-base font-medium text-[#3730A3] underline"
+                                className={`text-base font-medium underline ${theme === 'dark' ? 'text-[#E1E1FF]' : 'text-[#3730A3]'}`}
                             >
                                 {data?.siteView}
                             </a>
