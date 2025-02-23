3e91518dee7da1f553d6507f5b19a9220c329705
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 12:39:31 +0300

    Skills component organised
    Il componente Skills recupera i dati corretti in base alla selezione della lingua.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,26 +10,25 @@ const Skills = () => {
     if (error) return <p className="text-center text-red-500">Error: {error}</p>;
 
     return (
-        <div className={` ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
-            <h2 className={`text-5xl font-semibold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#34371f]'} text-left`}>
-                {data?.skills?.title}
+        <div className={` mt-16 ${theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"}`}>
+            <h2 className={`text-5xl font-semibold ${theme === "dark" ? "text-[#AEBCCF]" : "text-[#34371f]"}`}>
+                {data?.title}
             </h2>
 
-
-            <div className="flex gap-x-50 mt-8">
-                {data?.map((skill, index) => (
+            <div className="flex gap-x-12 mt-8">
+                {data.items?.map((skill, index) => (
                     <div key={index} className="w-1/3 text-left">
-                        <h3 className={`text-3xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#4338CA]'} text-left`}>
+                        <h3 className={`text-3xl font-medium ${theme === "dark" ? "text-[#B7AAFF]" : "text-[#4338CA]"}`}>
                             {skill.name}
                         </h3>
-                        <p className={`text-xs ${theme === 'dark' ? 'text-[#FFFFFF]' : 'text-[#6B7280]'} mt-2 text-left overflow-hidden line-clamp-3`}>
+                        <p className={`text-xs ${theme === "dark" ? "text-white" : "text-gray-600"} mt-2 line-clamp-3`}>
                             {skill.description}
                         </p>
                     </div>
                 ))}
             </div>
 
-            <div className={`mt-15 mb-5 border-b-2 ${theme === 'dark' ? 'border-[#BAB2E]' : 'border-[#BAB2E7]'}`}></div>
+            <div className={`mt-12 mb-12 border-b-2 ${theme === "dark" ? "border-[#BAB2E7]" : "border-[#BAB2E7]"}`}></div>
         </div>
     );
 };
