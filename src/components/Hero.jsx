c1869c25fa9dde3ebe75409eeeb79a5cb780a115
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-23 16:36:33 +0300

    component is updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 import { Github, Linkedin } from 'lucide-react';
 import useFetch from '../hooks/useFetch';
 import ppImage from '../assets/pp.jpg';
+import { useTheme } from "../context/ThemeContext";
 
 const Hero = () => {
-    const { data } = useFetch('/', 'hero')
-
+    const { data } = useFetch('/', 'hero');
+    const { theme } = useTheme();
 
     return (
         <div className="container mx-auto flex items-center justify-between py-16 px-4">
             <div className="flex flex-col space-y-4">
                 <div className="flex items-center space-x-4">
                     <div className="w-16 h-1 bg-[#3730A3]"></div>
-                    <h2 className="text-xl font-medium text-[#3730A3] leading-tight">{data?.name}</h2>
+                    <h2 className={`text-xl font-medium ${theme === 'dark' ? 'text-[#B7AAFF]' : 'text-[#3730A3]'} leading-tight`}>
+                        {data?.name}
+                    </h2>
                 </div>
 
-                <h3 className="text-[60px] leading-tight font-bold text-[#1F2937] ">{data?.heading}</h3>
-                <h3 className="text-[60px] leading-tight font-bold text-[#1F2937]">{data?.heading2}</h3>
+                <h3 className={`text-[60px] leading-tight font-bold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#1F2937]'} `}>
+                    {data?.heading}
+                </h3>
+                <h3 className={`text-[60px] leading-tight font-bold ${theme === 'dark' ? 'text-[#AEBCCF]' : 'text-[#1F2937]'} `}>
+                    {data?.heading2}
+                </h3>
 
-
-                <p className="text-[#6B7280] text-lg">{data?.aboutMe}</p>
+                <p className={`text-lg ${theme === 'dark' ? 'text-[#FFFFFF]' : 'text-[#6B7280]'} `}>
+                    {data?.aboutMe}
+                </p>
 
                 <div className="flex space-x-4 mt-6">
-                    <button className="py-2 px-6 text-lg font-semibold text-white bg-[#3730A3] rounded-md">
+                    <button className={`py-2 px-6 text-lg font-semibold ${theme === 'dark' ? 'bg-[#E1E1FF] text-[#000000]' : 'bg-[#3730A3] text-white'} rounded-md`}>
                         {data?.heroHire}
                     </button>
 
@@ -30,9 +38,9 @@ const Hero = () => {
                         href="https://github.com/mustafa-bircan"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="py-2 px-6 text-lg font-semibold border border-[#3730A3] rounded-md text-[#3730A3] flex items-center space-x-2 hover:bg-[#3730A3] hover:text-white transition"
+                        className={`py-2 px-6 text-lg font-semibold ${theme === 'dark' ? 'bg-[#383838] text-[#E1E1FF]' : 'bg-[#3730A3] text-[#3730A3]'}  rounded-md flex items-center space-x-2 hover:bg-[#3730A3] hover:text-white transition`}
                     >
-                        <Github className="w-5 h-5" />
+                        <Github className={`w-5 h-5 ${theme === 'dark' ? 'text-[#BAB2E7]' : ''}`} />
                         <span>GitHub</span>
                     </a>
 
@@ -40,9 +48,9 @@ const Hero = () => {
                         href="https://github.com/mustafa-bircan"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="py-2 px-6 text-lg font-semibold border border-[#3730A3] rounded-md text-[#3730A3] flex items-center space-x-2 hover:bg-[#3730A3] hover:text-white transition"
+                        className={`py-2 px-6 text-lg font-semibold ${theme === 'dark' ? 'bg-[#383838] text-[#E1E1FF]' : 'bg-[#3730A3] text-[#3730A3]'}  rounded-md flex items-center space-x-2 hover:bg-[#3730A3] hover:text-white transition`}
                     >
-                        <Linkedin className="w-5 h-5" />
+                        <Linkedin className={`w-5 h-5 ${theme === 'dark' ? 'text-[#BAB2E7]' : ''}`} />
                         <span>LinkedIn</span>
                     </a>
                 </div>
@@ -54,8 +62,6 @@ const Hero = () => {
                     className="max-w-[60%] max-h-[50%] object-contain rounded-[16px] shadow-lg"
                 />
             </div>
-
-
         </div>
     );
 };
