0b2bb6bcc87a0bf5cb3717a2d58b8c04865e27c1
mustafa-bircan <msbrtfcn@gmail.com>
2025-02-24 13:05:58 +0300

    revised component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,11 +3,8 @@ import useFetch from "../hooks/useFetch";
 import { useTheme } from "../context/ThemeContext";
 
 const Skills = () => {
-    const { data, loading, error } = useFetch("/", "skills");
+    const { data } = useFetch("/", "skills");
     const { theme } = useTheme();
-
-    if (loading) return <p className="text-center text-gray-600">Loading...</p>;
-    if (error) return <p className="text-center text-red-500">Error: {error}</p>;
 
     return (
         <div id="skills-section" className="mt-16" >
@@ -16,13 +13,13 @@ const Skills = () => {
             </h2>
 
             <div className="flex gap-x-12 mt-8">
-                {data.items?.map((skill, index) => (
+                {data?.items?.map((skill, index) => (
                     <div key={index} className="w-1/3 text-left">
                         <h3 className={`text-3xl font-medium ${theme === "dark" ? "text-[#B7AAFF]" : "text-[#4338CA]"}`}>
-                            {skill.name}
+                            {skill?.name}
                         </h3>
                         <p className={`text-xs ${theme === "dark" ? "text-white" : "text-gray-600"} mt-2 line-clamp-3`}>
-                            {skill.description}
+                            {skill?.description}
                         </p>
                     </div>
                 ))}
